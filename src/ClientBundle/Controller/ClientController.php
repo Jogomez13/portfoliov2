993e47ef87b1507aafcc57c63ae0916638e54d5e
Jogomez13 <jonathan.gomez.web@gmail.com>
2017-06-15 20:07:25 +0200

    implementation du front cote visiteur css + js

--- src/ClientBundle/Controller/ClientController.php
+++ src/ClientBundle/Controller/ClientController.php
@@ -8,10 +8,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ClientController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
         return $this->render('ClientBundle:Default:index.html.twig');
+    }
+    
+     /**
+     * @Route("/about", name ="about")
+     */
+    public function getAbout()
+    {
+        return $this->render('ClientBundle:Default:about.html.twig');
+    }
+    
+     /**
+     * @Route("/blog", name ="blog")
+     */
+    public function getBlog()
+    {
+        return $this->render('ClientBundle:Default:blog.html.twig');
+    }
+    
+      /**
+     * @Route("/post", name ="post")
+     */
+    public function getPost()
+    {
+        return $this->render('ClientBundle:Default:post.html.twig');
     }
 }
